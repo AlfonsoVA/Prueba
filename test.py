e2dd21307161617c5109231a38aa2f48ddc12282
AlfonsoVA <alfonsova@ciencias.unam.mx>
2016-08-30 18:59:37 -0500

    Nothing new

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ import unittest
 from nose.tools import eq_
 from nose.tools import ok_
 from nose.tools import timed
-from numbers import number, rebmun
+from numbers import numero, negativo
 
 
 class TestNumbers(unittest.TestCase):
     def test_additional(self):
-        eq_(number(25) + number(24) + rebmun(7), 42) #Eq se asegura que los valores sean iguales #number lo pasa a numero, rebmun lo pasa a negativo o vicevera
+        eq_(numero(25) + numero(24) + negativo(7), 42) #Eq se asegura que los valores sean iguales #number lo pasa a numero, rebmun lo pasa a negativo o vicevera
     def testo(self):
-   		ok_(number(25) + number(24) + rebmun(7), 1000)
+   		ok_(numero(25) + numero(24) + negativo(7), 1000)
     def test_trivial(self):
-   	    ok_(number(25) + number(24) + rebmun(7), 42) #Ok lo hace de manera similar que eq   	    
+   	    ok_(numero(25) + numero(24) + negativo(7), 42) #Ok lo hace de manera similar que eq   	    
 
 if __name__ == '__main__':
     unittest.main()
