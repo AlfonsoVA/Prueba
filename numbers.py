cf28b4943fddf4c4223c948680a99900c517788b
AlfonsoVA <alfonsova@ciencias.unam.mx>
2016-08-29 23:12:38 -0500

    modificado

--- numbers.py
+++ numbers.py
@@ -1,16 +1,20 @@
 import sys
-import warnings
+import warnings 
 
 __version__ = '1.0'
 
 warnings.warn('You are an idiot')
-
+print("hey lenguage!")
 
 def number(value):
-    if value > sys.maxint:
-        return 'long int is long'
+    if value > sys.maxsize: #Cambiando maxint por maxsize
+        print(str(value) + " long int is long")
+        warnings.warn('.__.')
+        return False
     elif value > 9000:
-        return 'It\'s over 9000!'
+    	print(str(value) + " It\'s over 9000!")
+    	warnings.warn('.__.')
+    	return False
     return int(value)
 
 
