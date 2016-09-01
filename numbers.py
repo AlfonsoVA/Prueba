929975b1f7774954cbd1846d712c4f424390ddf4
AlfonsoVA <alfonsova@ciencias.unam.mx>
2016-08-31 20:20:12 -0500

    Metodo prueba mejorado

--- numbers.py
+++ numbers.py
@@ -2,45 +2,47 @@ import sys
 import warnings 
 
 __version__ = '1.0'
-print("hola")
 
 # numbers.py Se basa en pasar los valores a numeros o numeros negativos para luego operar
 #con ellos con pruebas unitarias
 def numero(value):
     if value > sys.maxsize: #Cambiando maxint por maxsize
         print(str(value) + " el numero es muy largo")
-        warnings.warn('.__.')
         return False
     elif value > 8000:
     	print(str(value) + " Es mas de 8000!")
-    	warnings.warn('.__.')
     	return False
-    return int(value)
+    return int(value)    
 
 
 def negativo(value):
-    return - number(value)
+    return - numero(value)
     
 #Creando una prueba simple del programa
 def prueba():
-	print("Hola, a continuacion de 2 numeros")
-	n=input()
-	m=input()
+	print("Hola, a continuacion de 2 numeros separados por un SPACE")
+	x=input()
+	x.split(" ")
+	n=x[0]
+	m=x[2]
 	print("Sus numeros son: " + str(n) +" "+ str(m))
 	print ("Selecciones una operacion simple")
 	print ("Suma: s // Resta: r // Multiplicacion: m // Division: d")
 	o=input()
 	if str(o) == "s":
-		print( numero(int(n)+int(m)))
+		x= numero(int(n)+int(m))
 	if str(o) == "r":
-		print( numero(int(n)-int(m)))
+		x= numero(int(n)-int(m))
 	if str(o) == "m":
-		print( numero(int(n)*int(m)))
+		x= numero(int(n)*int(m))
 	if str(o) == "d":
 		if int(m)!=0:
-			print( numero(int(n)/int(m)))
-
-prueba()
+			x= numero(int(n)/int(m))
+		else:
+			print("El divisor no puede ser cero")
+	return x
+	
+print(prueba())
 
 def leet():
     return 31337
